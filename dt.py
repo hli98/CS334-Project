407ba6d6290fc87c3fdf73d480daa5bcabc6493f
Helin Li <lihelin1998@gmail.con>
2020-12-07 02:41:10 +0800

    Updated metrics

--- dt.py
+++ dt.py
@@ -2,18 +2,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import tree
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
 
 
 def main():
-    #read data
-    x = pd.read_csv("train_x.csv")
-    fn = x.columns.tolist()
-    x = x.to_numpy()
-    y = pd.read_csv("train_y.csv").to_numpy()
-    #print("y length",len(y))
-    #hide next line, undo it when generate formal output
-    #train_info = pd.read_csv("train_info.csv").to_numpy()
+    #Read Data
+    #train_x = pd.read_csv("train_x.csv")
+    #fn = train_x.columns.tolist()
+    #train_x = train_x.to_numpy()
+    train_x = pd.read_csv("train_x.csv").to_numpy()
+    train_y = pd.read_csv("train_y.csv").to_numpy()
+    test_x = pd.read_csv("test_x.csv").to_numpy()
+    test_y = pd.read_csv("test_y.csv").to_numpy()
 
+    '''
     #look for row indices where there is no prior order
     row_indices = []
     row_index = 0
@@ -22,29 +25,47 @@ def main():
         if row[4] == -1:
             row_indices.append(row_index)
         row_index += 1
-
-
+    '''
+    '''
     #Train-Test split   
     train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=0.33, random_state=42)
-
+    '''
 
 
     #Decision Tree
-    dtclf = tree.DecisionTreeClassifier(max_depth=26, min_samples_leaf=10000)
-    dtclf.fit(train_x, train_y.ravel())
-    dtyhat = dtclf.predict(train_x)
-    for i in range(len(dtyhat)):
-        if i in row_indices:
-            dtyhat[i] = 0
-    pd.DataFrame(dtyhat).to_csv("dt_yhat.csv", index = False)
-    print("dt y length",len(dtyhat))
+    clf = tree.DecisionTreeClassifier()#(max_depth=26)#, min_samples_leaf=10000)
+    clf.fit(train_x, train_y.ravel())
+    train_yhat = clf.predict(train_x)
+    test_yhat = clf.predict(test_x)
 
+    #Metrics
+    train_acc = accuracy_score(train_y, train_yhat)
+    test_acc = accuracy_score(test_y, test_yhat)
+    train_f1 = f1_score(train_y, train_yhat)
+    test_f1 = f1_score(test_y, test_yhat)
+    
+
+    #Printing out Metrics
+    print("train_acc", train_acc)
+    print("test_acc", test_acc)
+    print("train_f1", train_f1)
+    print("test_f1", test_f1)
+
+
+
+    #for i in range(len(dtyhat)):
+        #if i in row_indices:
+            #dtyhat[i] = 0
+    #pd.DataFrame(train_yhat).to_csv("train_yhat.csv", index = False)
+    #print("train y length",len(train_yhat))
+
+    '''
     #Plot DT
     fig, axes = plt.subplots(nrows = 1,ncols = 1, dpi=100, figsize=(12, 12))
     tree.plot_tree(dtclf, feature_names = fn, ax=axes)
     plt.savefig("dt.png", fontsize=12)
     plt.show()
-
+    '''
 
 
 if __name__ == "__main__":
--- rf.py
+++ rf.py
@@ -1,42 +1,38 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn import tree
 from sklearn import ensemble
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
 
 
 def main():
     #read data
-    x = pd.read_csv("train_x.csv").to_numpy()
-    y = pd.read_csv("train_y.csv").to_numpy()
-    #print("y length",len(y))
-    #hide next line, undo it when generate formal output
-    #train_info = pd.read_csv("train_info.csv").to_numpy()
-
-    #look for row indices where there is no prior order
-    row_indices = []
-    row_index = 0
-    #Actually, every order in the training dataset has a prior order
-    for row in x:
-        if row[4] == -1:
-            row_indices.append(row_index)
-        row_index += 1
-
-
-    #Train-Test split   
-    train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=0.33, random_state=42)
+    train_x = pd.read_csv("train_x.csv").to_numpy()
+    train_y = pd.read_csv("train_y.csv").to_numpy()
+    test_x = pd.read_csv("test_x.csv").to_numpy()
+    test_y = pd.read_csv("test_y.csv").to_numpy()
 
     #Random Forest
-    rfclf = ensemble.RandomForestClassifier()
-    rfclf.fit(train_x, train_y.ravel())
-    rfyhat = rfclf.predict(train_x)
-    for i in range(len(rfyhat)):
-        if i in row_indices:
-            rfyhat[i] = 0
-    pd.DataFrame(rfyhat).to_csv("rf_yhat.csv", index = False)
-    print("rf y length",len(rfyhat))
+    clf = ensemble.RandomForestClassifier()
+    clf.fit(train_x, train_y.ravel())
+    train_yhat = clf.predict(train_x)
+    test_yhat = clf.predict(test_x)
 
-    #Plot RF
+    #Metrics
+    train_acc = accuracy_score(train_y, train_yhat)
+    test_acc = accuracy_score(test_y, test_yhat)
+    train_f1 = f1_score(train_y, train_yhat)
+    test_f1 = f1_score(test_y, test_yhat)
+    
+
+    #Printing out Metrics
+    print("train_acc", train_acc)
+    print("test_acc", test_acc)
+    print("train_f1", train_f1)
+    print("test_f1", test_f1)
+
+    '''
     #Showing the first 5 rfs
     fn = train_x.columns.values.tolist()
     fig, axes = plt.subplots(nrows = 1,ncols = 5,figsize = (10,2), dpi=900)
@@ -47,7 +43,7 @@ def main():
                     ax = axes[index])
         axes[index].set_title('Estimator: ' + str(index), fontsize = 11)
     plt.show()
-
+    '''
 
 
 
